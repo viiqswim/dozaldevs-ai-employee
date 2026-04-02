feafb529f994356bc3e01a208816e7418cdcb27a
Victor Dozal <v.dozal@live.com>
2026-04-02 00:26:09 -0500

    chore: add Supabase Docker Compose with ai_employee DB

---
+++ docker/volumes/db/ai_employee_grants.sql
@@ -0,0 +1,9 @@
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO supabase_storage_admin;
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO supabase_auth_admin;
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO authenticator;
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO anon;
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO authenticated;
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO service_role;
+GRANT ALL PRIVILEGES ON DATABASE ai_employee TO supabase_admin;
+GRANT CREATE ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO postgres;
