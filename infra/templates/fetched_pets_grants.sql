17f1ef35331ddd853a1961c692702db81454fb48
Victor Dozal <v.dozal@live.com>
2026-04-07 18:31:33 -0500

    chore(infra): relocate compose templates to infra/templates and gitignore drafts

---
+++ infra/templates/fetched_pets_grants.sql
@@ -0,0 +1,9 @@
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO supabase_storage_admin;
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO supabase_auth_admin;
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO authenticator;
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO anon;
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO authenticated;
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO service_role;
+GRANT ALL PRIVILEGES ON DATABASE fetched_pets TO supabase_admin;
+GRANT CREATE ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO postgres;
---
+++ infra/templates/vlre_hub_grants.sql
@@ -0,0 +1,9 @@
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO supabase_storage_admin;
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO supabase_auth_admin;
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO authenticator;
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO anon;
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO authenticated;
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO service_role;
+GRANT ALL PRIVILEGES ON DATABASE vlre_hub TO supabase_admin;
+GRANT CREATE ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO postgres;
